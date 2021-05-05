e1df1fda46f44d5c4633665fd47bf3feec4037e3
akshay-hingarajiya <83569095+akshay-hingarajiya@users.noreply.github.com>
2021-05-05 16:05:24 +0530

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
+package helloworld;
+ 
 public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+ 
+   public static void main(String[] args) {
+ 
+        System.out.println("Hello World!");
+   }
 }
