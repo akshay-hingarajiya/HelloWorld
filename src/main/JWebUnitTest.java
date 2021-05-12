6409ea99a9eef4df978d17b23b97671d5d8b2ad4
akshay-hingarajiya <83569095+akshay-hingarajiya@users.noreply.github.com>
2021-05-12 15:15:59 +0530

    Add files via upload

---
+++ src/main/JWebUnitTest.java
@@ -0,0 +1,37 @@
+package junitwebapp;
+import static net.sourceforge.jwebunit.junit.JWebUnit.assertLinkPresent;
+import static net.sourceforge.jwebunit.junit.JWebUnit.assertTitleEquals;
+import static net.sourceforge.jwebunit.junit.JWebUnit.beginAt;
+import static net.sourceforge.jwebunit.junit.JWebUnit.clickLink;
+import static net.sourceforge.jwebunit.junit.JWebUnit.setBaseUrl;
+import static net.sourceforge.jwebunit.junit.JWebUnit.setTestingEngineKey;
+ 
+import org.junit.Before;
+import org.junit.Test;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
+ 
+import net.sourceforge.jwebunit.util.TestingEngineRegistry;
+
+
+public class JWebUnitTest {
+    @Before
+    public void prepare() {
+        setTestingEngineKey(TestingEngineRegistry.TESTING_ENGINE_HTMLUNIT); 
+        setBaseUrl("http://localhost:8081/junitwebapp");
+    }
+ 
+ @BeforeAll
+void beforeClass()
+{
+ system.out.println("running......beforeall");
+}
+ 
+    @Test
+    public void testLoginPage() {
+        beginAt("index.jsp"); 
+        assertTitleEquals("Hello AK");
+    }
+
+}
