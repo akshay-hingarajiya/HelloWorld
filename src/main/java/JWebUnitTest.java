9e5d08b060e5dfa24ea0c8dd768143e2b26b0f54
akshay-hingarajiya <83569095+akshay-hingarajiya@users.noreply.github.com>
2021-05-13 11:29:23 +0530

    Update JWebUnitTest.java

--- src/main/java/JWebUnitTest.java
+++ src/main/java/JWebUnitTest.java
@@ -4,7 +4,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import net.sourceforge.jwebunit.junit.JWebUnit;
+//import net.sourceforge.jwebunit.junit.JWebUnit;
 import static net.sourceforge.jwebunit.junit.JWebUnit.assertLinkPresent;
 import static net.sourceforge.jwebunit.junit.JWebUnit.assertTitleEquals;
 import static net.sourceforge.jwebunit.junit.JWebUnit.beginAt;
