cb2b4de74316ebc4e5c5f26c4d2b01a483bdfc3f
akshay-hingarajiya <83569095+akshay-hingarajiya@users.noreply.github.com>
2021-05-13 15:44:19 +0530

    Update JWebUnitTest.java

--- src/test/java/JWebUnitTest.java
+++ src/test/java/JWebUnitTest.java
@@ -13,9 +13,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
-import net.sourceforge.jwebunit.junit.JWebUnit.beginAt;
 
-import net.sourceforge.jwebunit.junit.JWebUnit;
+import static net.sourceforge.jwebunit.junit.JWebUnit;
 /*
 import static net.sourceforge.jwebunit.junit.JWebUnit.assertLinkPresent;
 import static net.sourceforge.jwebunit.junit.JWebUnit.assertTitleEquals;
