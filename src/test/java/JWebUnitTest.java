c98341b22b5bc8f56ffa519295c7519aff3ee884
akshay-hingarajiya <83569095+akshay-hingarajiya@users.noreply.github.com>
2021-05-14 16:26:53 +0530

    Update JWebUnitTest.java

--- src/test/java/JWebUnitTest.java
+++ src/test/java/JWebUnitTest.java
@@ -27,14 +27,14 @@ import net.sourceforge.jwebunit.util.TestingEngineRegistry;
 
 
 public class JWebUnitTest {
-	@Before
+	/*@Before
 	public void prepare() {
 		//setBaseUrl("http://http://localhost:9090/JenkinsWar");
 		// WebDriver driver = new FirefoxDriver();
     		//driver.get("http://http://localhost:9090/JenkinsWar");
 		setTestingEngineKey(TestingEngineRegistry.TESTING_ENGINE_HTMLUNIT); 
-		setBaseUrl("http://localhost:9090/manager/html/JenkinsWar");
-	}
+		setBaseUrl("http://localhost:9090/JenkinsWar");
+	}*/
  
 	
 @BeforeAll
