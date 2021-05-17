5e888b3192eda3e5f7ef049f14413c49d0493655
akshay-hingarajiya <83569095+akshay-hingarajiya@users.noreply.github.com>
2021-05-17 09:11:32 +0530

    Update JWebUnitTest.java

--- src/main/test/java/JWebUnitTest.java
+++ src/main/test/java/JWebUnitTest.java
@@ -50,7 +50,7 @@ public class JWebUnitTest {
 	//tester.setResourceBase("./src/main/webapp");
 	//tester.addServlet(jspServlet.class,"*.jsp");
 	tester.setTestingEngineKey(TestingEngineRegistry.TESTING_ENGINE_HTMLUNIT); 
-        tester.setBaseUrl("http://localhost:8081");
+        tester.setBaseUrl("http://localhost:9090/JenkinsWar");
     }
 
 	
